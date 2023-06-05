37bf7eb9c55a89b2bac5461070106ffb4b5aedeb
Kent Ho <ho.kimcang@gmail.com>
2023-06-05 19:50:16 -0400

    add custom screen theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      'tablet': '768px',
+      'desktop': '1280px'
+    },
     colors: {
       'white': "#fff",
       'white-dark': "#f4f4f4",
