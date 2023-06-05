f33b695130a081e2e04bdb245aecadd364d6f5b8
Kent Ho <ho.kimcang@gmail.com>
2023-06-05 18:49:42 -0400

    create custom colour theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      'white': "#fff",
+      'white-dark': "#f4f4f4",
+      "gray-light": "7A7A7A",
+      "gray-dark": "#383838",
+      "blue-light": "#1a6baf",
+      "blue-dark": "#15568c",
+      "green-light": "#7fc243",
+      "green-dark": "#669b36",
+      "orange-light": "#ef8129",
+      "orange-dark": "#f78931",
+    },
   },
   plugins: [],
 };
