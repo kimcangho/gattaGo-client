1a2b2b09c43eef88cd27ea784a3a32a2a409a542
Kent Ho <ho.kimcang@gmail.com>
2023-06-06 12:27:25 -0400

    create responsive design for signup page with mobile and tablet/desktop views

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
 import gattaGoLogo from "../assets/logos/gattaGo-boat.svg";
 
 interface HeaderProps {}
 
 const Header = ({}: HeaderProps): JSX.Element => {
   return (
-    <div className="bg-white flex justify-center items-center border border-gray-border px-6 py-1 tablet:p-4 space-x-2 tablet:space-x-3">
-      <img src={gattaGoLogo} alt="gattaGo Logo" className="h-6 tablet:h-10" />
-      <h2 className="text-2xl tablet:text-3xl">gattaGo</h2>
+    <div className="bg-white flex justify-center items-center border border-gray-border px-6 py-1 tablet:p-4">
+      <Link to="../" className="flex justify-center items-center space-x-2 tablet:space-x-3">
+        <img src={gattaGoLogo} alt="gattaGo Logo" className="h-6 tablet:h-10" />
+        <h2 className="text-gray-dark text-2xl tablet:text-3xl">gattaGo</h2>
+      </Link>
     </div>
   );
 };
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,7 +1,44 @@
+import { Link } from "react-router-dom";
+
 interface SignupProps {}
 
 const SignupPage = ({}: SignupProps): JSX.Element => {
-  return <div>SignupPage</div>;
+  return (
+    <div className="flex-col text-center items-center w-full tablet:w-[768px] mx-auto py-5 tablet:py-6">
+      <h3 className="my-4 tablet:mt-10 tablet:mb-5 tablet:text-2xl">
+        Enter your email address
+      </h3>
+      <p className="m-5 mt-2.5 tablet:mb-8">
+        To begin the sign-up process, please provide your email address below!
+      </p>
+
+      <form className="bg-white flex flex-col space-y-2 tablet:space-y-4 mb-5 p-2.5 tablet:p-6 border border-gray-border tablet:rounded">
+        <input
+          type="text"
+          placeholder="Input email address"
+          className="text-yellow border border-gray-border outline-1 focus:outline-blue-light rounded px-2 py-4"
+          required
+        />
+        <button
+          type="submit"
+          className="bg-green-light text-white rounded hover:bg-green-dark p-4"
+        >
+          Continue
+        </button>
+      </form>
+
+      <p className="mb-2.5">
+        <b>Already have an account? </b>
+        <span className="text-blue-light hover:underline">
+          <Link to="../login">Log in here!</Link>
+        </span>
+      </p>
+      <p className="mt-4 mb-2.5">
+        gattaGo is an online dragonboat team manager used to organize regattas,
+        race progressions, lineups and athletes.
+      </p>
+    </div>
+  );
 };
 
 export default SignupPage;
