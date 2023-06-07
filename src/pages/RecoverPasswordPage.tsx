e3fe7a726a87f152f28c9e142bf7e4e902d7ca10
Kent Ho <ho.kimcang@gmail.com>
2023-06-07 16:08:19 -0400

    include react-hook-form and navigation in recover password page

--- src/pages/RecoverPasswordPage.tsx
+++ src/pages/RecoverPasswordPage.tsx
@@ -1,48 +1,90 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 interface RecoverPasswordProps {
   email: String;
   setEmail: Function;
 }
 
-const RecoverPasswordPage = ({}: RecoverPasswordProps): JSX.Element => {
+const RecoverPasswordPage = ({
+  email,
+  setEmail,
+}: RecoverPasswordProps): JSX.Element => {
+  const [isPasswordSent, setIsPasswordSent] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email,
+    },
+  });
   return (
     <div className="w-full tablet:w-[448px] mx-auto">
       <h3 className="text-center my-4 tablet:mt-10 tablet:mb-5 tablet:text-2xl">
         Recover Your Password
       </h3>
-      <p className="m-5 mt-2.5 tablet:mb-8">
-        Enter the email you signed up with and we'll send a password reset!
+
+      <p className="m-5 mt-2.5 tablet:mb-8 text-center">
+        {isPasswordSent
+          ? "Password reset sent!"
+          : "Enter the email you signed up with and we'll send a password reset!"}
       </p>
-      <form className="max-w-[448px] m-auto p-6 bg-white border border-gray-border rounded">
-        <div className="flex flex-col mb-2.5">
-          <label htmlFor="email" className="mb-1 font-bold">
-            Email
-          </label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            placeholder="Input email address"
-            className="mb-1 px-2 py-2.5 bg-white-dark border border-gray-border rounded focus:outline-blue-light"
-            required
-          />
-          <p className="text-xs">
-            Make sure this is the email you signed up with!
-          </p>
-        </div>
-        <div className="flex flex-row space-x-2 tablet:space-x-3 mt-4">
-          <button className="p-4 w-full text-white bg-green-light hover:bg-green-dark rounded">
-            Reset Password
-          </button>
-          <Link
-            to="../login"
-            className="p-4 w-full text-center text-white bg-orange-light hover:bg-orange-dark rounded"
-          >
-            <div>Cancel</div>
-          </Link>
-        </div>
-      </form>
+      {!isPasswordSent ? (
+        <form
+          onSubmit={handleSubmit(({ email }) => {
+            setEmail(email);
+            setIsPasswordSent(true);
+          })}
+          className="max-w-[448px] m-auto p-6 bg-white border border-gray-border rounded"
+        >
+          <div className="flex flex-col mb-2.5">
+            <label htmlFor="email" className="mb-1 font-bold">
+              Email
+            </label>
+            <input
+              type="text"
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "Email field can't be empty!",
+                },
+                pattern: {
+                  value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+                  message: "Field needs to be an email!",
+                },
+              })}
+              id="email"
+              placeholder="Input email address"
+              className="mb-1 px-2 py-2.5 bg-white-dark border border-gray-border rounded focus:outline-blue-light"
+            />
+            <p className="text-xs">
+              Make sure this is the email you signed up with!
+            </p>
+            <p className="text-red-500 text-left">{errors.email?.message}</p>
+          </div>
+          <div className="flex flex-row space-x-2 tablet:space-x-3 mt-4">
+            <button className="p-4 w-full text-white bg-green-light hover:bg-green-dark rounded">
+              <p>Reset Password</p>
+            </button>
+            <Link
+              to="../login"
+              className="p-4 w-full text-center text-white bg-orange-light hover:bg-orange-dark rounded"
+            >
+              <p>Cancel</p>
+            </Link>
+          </div>
+        </form>
+      ) : (
+        <Link
+          to="../login"
+          className="flex justify-center max-w-[448px] p-4 text-white bg-green-light hover:bg-green-dark rounded"
+        >
+          Back to Login Page
+        </Link>
+      )}
     </div>
   );
 };
