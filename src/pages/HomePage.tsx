53df681f4fa01d55eacae592ba93d187ce40004a
Kent Ho <ho.kimcang@gmail.com>
2023-06-07 14:54:22 -0400

    add react-hook-form to signup page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 interface HomeProps {
   email: string;
   setEmail: Function;
 }
 
-const HomePage = ({}: HomeProps): JSX.Element => {
-  return <div></div>;
+const HomePage = ({ email }: HomeProps): JSX.Element => {
+  return <Link to="../signup">{email}</Link>;
 };
 
 export default HomePage;
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,11 +1,22 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 interface SignupProps {
   email: string;
   setEmail: Function;
 }
 
-const SignupPage = ({}: SignupProps): JSX.Element => {
+const SignupPage = ({ email, setEmail }: SignupProps): JSX.Element => {
+  const {
+    register, //  used to register a form field/input into react-hook-forms
+    handleSubmit, //  executes on form submission
+    formState: { errors }, //  adds errors property to form state
+  } = useForm({
+    defaultValues: {
+      email,
+    },
+  });
+
   return (
     <div className="flex-col text-center items-center w-full tablet:w-[768px] mx-auto py-5 tablet:py-6">
       <h3 className="my-4 tablet:mt-10 tablet:mb-5 tablet:text-2xl">
@@ -15,14 +26,29 @@ const SignupPage = ({}: SignupProps): JSX.Element => {
         To begin the sign-up process, please provide your email address below!
       </p>
 
-      <form className="bg-white flex flex-col space-y-2 tablet:space-y-4 mb-5 p-2.5 tablet:p-6 border border-gray-border tablet:rounded">
+      <form
+        onSubmit={handleSubmit((data) => {
+          setEmail(data?.email);
+        })}
+        className="bg-white flex flex-col space-y-2 tablet:space-y-4 mb-5 p-2.5 tablet:p-6 border border-gray-border tablet:rounded"
+      >
         <input
           type="text"
-          name="email"
+          {...register("email", {
+            required: {
+              value: true,
+              message: "Field can't be empty!",
+            },
+            pattern: {
+              value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+              message: "Field needs to be an email!",
+            },
+          })}
           id="email"
           placeholder="Input email address"
           className="bg-white-dark text-yellow border border-gray-border outline-1 focus:outline-blue-light rounded px-2 py-4"
         />
+        <p className="text-red-500 text-left">{errors.email?.message}</p>
         <button
           type="submit"
           className="bg-green-light text-white rounded hover:bg-green-dark p-4"
