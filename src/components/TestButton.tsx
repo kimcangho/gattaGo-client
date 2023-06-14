aa2a55293a9bd3c5d9deeb8b8611df9cadb1d062
Kent Ho <ho.kimcang@gmail.com>
2023-06-14 11:33:27 -0400

    include test button

---
+++ src/components/TestButton.tsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+// type Props = {};
+
+const TestButton = () => {
+  const testRequest = async () => {
+    console.log("Testing axios request...");
+    const {data} = await axios.post("http://localhost:7777/token", {
+      email: "test@email.com",
+      password: "password",
+      refreshToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAZW1haWwuY29tIiwiaWF0IjoxNjg2NzU1MTM3fQ._FoYLBYMYC1TBCNLv6C2RW7nt0nHfHjW7Tptn3JP140"
+    });
+    console.log(data);
+  };
+
+  return <div onClick={testRequest}>TestButton</div>;
+};
+
+export default TestButton;
