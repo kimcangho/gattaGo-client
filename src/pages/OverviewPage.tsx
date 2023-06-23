c898a92a7370959c7be72c4a7abe90018150e0e6
Kent Ho <ho.kimcang@gmail.com>
2023-06-23 14:09:10 -0400

    build out tablet and desktop views for overview dashboard

--- src/pages/OverviewPage.tsx
+++ src/pages/OverviewPage.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import boatIcon from "../assets/icons/boat.svg";
+import editIcon from "../assets/icons/edit-entity.svg";
+import deleteIcon from "../assets/icons/delete-entity.svg";
 import createNew from "../assets/icons/create-new.svg";
 
 interface TeamData {
@@ -24,24 +27,44 @@ const OverviewPage = (): JSX.Element => {
     getAllTeams();
   }, []);
 
+  const navigate = useNavigate();
+
   const handleCreateTeam = async () => {
     //  To-do: Navigate to create team page
     console.log("Creating a new team...");
   };
 
   const handleSelectTeam = (event: any) => {
-    //  To-do: Link to team dashboard by id
+    //  To-do: Expand for team details  in mobile view
     console.log(event.currentTarget.id);
+  };
+
+  const handleEditTeam = (event: any) => {
+    event.stopPropagation();
+    navigate(`../${event.target.id}/dashboard`);
+  };
+
+  const handleDeleteTeam = (event: any) => {
+    //  To-do: Delete team from DB
+    event.stopPropagation();
+    console.log(`Delete team: ${event.target.id}`);
+    const currentTeams = myTeams.filter((team) => {
+      return team.id !== event.target.id;
+    });
+    setMyTeams(currentTeams);
   };
 
   return (
     <>
-      <div className="p-4 flex flex-col">
-        <div className="text-center p-2 bg-white border border-gray-border rounded-t w-full">
-          <h1>My Teams</h1>
-          {/* <h2 className="border rounded p-2 hover:text-white hover:bg-gray-light cursor-pointer">
-            New Team
-          </h2> */}
+      <div className="p-4 desktop:w-[1280px] desktop:mx-auto flex flex-col">
+        <div className="flex justify-between items-center text-center p-2 tablet:p-6 bg-white border border-gray-border rounded-t w-full">
+          <h1 className="tablet:text-4xl">My Teams</h1>
+          <h2
+            className="hidden tablet:block border-2 border-black rounded p-2 text-black hover:text-white hover:bg-gray-light tablet:text-2xl cursor-pointer"
+            onClick={handleCreateTeam}
+          >
+            Create New Team
+          </h2>
         </div>
 
         {myTeams.map((team) => {
@@ -50,20 +73,46 @@ const OverviewPage = (): JSX.Element => {
               key={team.id}
               id={team.id}
               onClick={handleSelectTeam}
-              className="flex space-x-2 items-center h-12 p-2 bg-white border-x border-b border-gray-border hover:bg-blue-wavy rounded-b cursor-pointer"
+              className="flex justify-between space-x-2 items-center h-12 p-2 tablet:p-6 bg-white border-x border-b border-gray-border hover:bg-blue-wavy rounded-b cursor-pointer"
             >
-              <img src={boatIcon} className="h-8" />
-              <h4>{team.name}</h4>
+              <div className="flex items-center space-x-2 tablet:space-x-6">
+                <img
+                  src={boatIcon}
+                  alt="Team Logo Placeholder"
+                  className="h-8 tablet:h-10"
+                />
+                <h4 className="truncate tablet:text-xl">{team.name}</h4>
+              </div>
+              <div className="flex items-center space-x-2">
+                <img
+                  src={editIcon}
+                  alt="Edit Team"
+                  className="h-6 tablet:h-8"
+                  id={team.id}
+                  onClick={handleEditTeam}
+                />
+                <img
+                  src={deleteIcon}
+                  alt="Delete Team"
+                  className="h-6 tablet:h-8"
+                  id={team.id}
+                  onClick={handleDeleteTeam}
+                />
+              </div>
             </div>
           );
         })}
 
         <div
-          className="flex space-x-2 items-center h-12 p-2 bg-white border-x border-b border-gray-border hover:bg-green-light rounded-b cursor-pointer"
+          className="tablet:hidden flex space-x-2 tablet:space-x-6 items-center p-2 tablet:px-6 bg-white border-x border-b border-gray-border hover:bg-green-light rounded-b cursor-pointer"
           onClick={handleCreateTeam}
         >
-          <img src={createNew} alt="Create New Team" className="h-full" />
-          <h4>Create new team</h4>
+          <img
+            src={createNew}
+            alt="Create New Team"
+            className="h-6 tablet:h-10"
+          />
+          <h4 className="tablet:text-xl">Create new team</h4>
         </div>
       </div>
     </>
