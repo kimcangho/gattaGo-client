af45d30e9663d58c6b095a3152e9bb54c94fa152
Kent Ho <ho.kimcang@gmail.com>
2023-07-16 21:31:28 -0400

    create mobile view dropdown list for active lineup

--- src/pages/LineupsPage.tsx
+++ src/pages/LineupsPage.tsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import useAxiosPrivate from "../hooks/usePrivateInterceptors";
 import useLogoutRedirect from "../hooks/useLogoutRedirect";
 import { LineupData } from "../interfaces/EntityData";
@@ -11,6 +11,7 @@ import chevronIconLeft from "../assets/icons/chevron-left.svg";
 
 const LineupsPage = (): JSX.Element => {
   const [teamLineups, setTeamLineups] = useState<LineupData | {}>({});
+  const [isLineupActive, setIsLineupActive] = useState<boolean>(false);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const { teamId } = useParams<string>();
   const axiosPrivate = useAxiosPrivate();
@@ -19,6 +20,7 @@ const LineupsPage = (): JSX.Element => {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors },
   } = useForm<CreateNewLineupFormData>({
     defaultValues: {
@@ -76,8 +78,24 @@ const LineupsPage = (): JSX.Element => {
     //   } catch (err) {}
   };
 
-  const handleToggleModal = () => {
-    console.log("Modal Toggled!");
+  const handleLineupStatus = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    if (event.target.value === "select") {
+      setValue("lineupName", "");
+      setIsModalOpen(false);
+      setIsLineupActive(false);
+    } else if (event.target.value === "new") {
+      setValue("lineupName", "");
+      setIsLineupActive(true);
+    } else {
+      setValue("lineupName", event.target.value);
+      setIsLineupActive(true);
+    }
+  };
+
+  const handleToggleModal = (): void => {
+    if (!isLineupActive) return;
     setIsModalOpen((prev) => !prev);
   };
 
@@ -95,7 +113,6 @@ const LineupsPage = (): JSX.Element => {
           )}
         </div>
       </div>
-
       <form
         onSubmit={handleSubmit(handleFormSubmit)}
         className="flex flex-col p-2 mb-2 tablet:p-6 max-w-[448px] mx-auto bg-white border border-gray-border rounded-t w-full"
@@ -110,56 +127,80 @@ const LineupsPage = (): JSX.Element => {
             id="activeLineup"
             defaultValue={"select"}
             className="px-2 py-2.5 bg-white-dark border border-gray-border rounded focus:outline-blue-light"
+            onChange={handleLineupStatus}
           >
             <option value="select">Select lineup</option>
             <option value="new">New lineup</option>
             {teamLineups &&
               teamLineups.lineups &&
               teamLineups.lineups.map((lineup) => {
-                return <option value={lineup.id}>{lineup.name}</option>;
+                return (
+                  <option key={lineup.id} value={lineup.name}>
+                    {lineup.name}
+                  </option>
+                );
               })}
           </select>
         </div>
 
-        <label htmlFor="lineupName">
-          <h3 className="text-blue-light">Lineup Name</h3>
-        </label>
-        <input
-          {...register("lineupName", {
-            required: {
-              value: true,
-              message: "Lineup name field can't be empty!",
-            },
-          })}
-          type="text"
-          id="lineupName"
-          name="lineupName"
-          placeholder="Input lineup name"
-          className="px-2 py-2.5 bg-white-dark border border-gray-border rounded focus:outline-blue-light"
-        />
-        {errors.lineupName && (
-          <p className="text-red-500">{errors.lineupName.message}</p>
+        {isLineupActive && (
+          <>
+            <label htmlFor="lineupName">
+              <h3 className="text-blue-light">Lineup Name</h3>
+            </label>
+            <input
+              {...register("lineupName", {
+                required: {
+                  value: true,
+                  message: "Lineup name field can't be empty!",
+                },
+              })}
+              type="text"
+              id="lineupName"
+              name="lineupName"
+              placeholder="Input lineup name"
+              className="px-2 py-2.5 bg-white-dark border border-gray-border rounded focus:outline-blue-light"
+            />
+            {errors.lineupName && (
+              <p className="text-red-500">{errors.lineupName.message}</p>
+            )}
+
+            <button
+              type="submit"
+              className="bg-green-light hover:bg-green-dark p-2 rounded border border-green-dark text-white"
+            >
+              Save Lineup
+            </button>
+          </>
         )}
-
-        <button
-          type="submit"
-          className="bg-green-light hover:bg-green-dark p-2 rounded border border-green-dark text-white"
-        >
-          Save Lineup
-        </button>
       </form>
-
+      {/* Modal Button */}
       <div
-        className="p-2 absolute bottom-[10%] left-0 border border-black rounded-r-lg bg-blue-wavy cursor-pointer"
+        className={`p-2 absolute bottom-[10%] ${
+          isModalOpen ? "right-0" : "left-0"
+        }  border border-black rounded-r-lg ${
+          !isLineupActive
+            ? "bg-gray-border cursor-not-allowed opacity-50"
+            : "bg-blue-wavy cursor-pointer"
+        }`}
         onClick={handleToggleModal}
       >
         <img src={rosterIcon} alt="Roster Icon" className="w-8 inline" />
         <img
-          src={isModalOpen ? chevronIconRight : chevronIconLeft}
-          alt={`Chevron ${isModalOpen ? chevronIconRight : chevronIconLeft}`}
+          src={isModalOpen ? chevronIconLeft : chevronIconRight}
+          alt={`Chevron ${isModalOpen ? chevronIconLeft : chevronIconRight}`}
           className="w-4 h-8 inline"
         />
       </div>
+      {/* Mobile View Modal */}
+
+      {isModalOpen && (
+        <div className="bg-slate-400 fixed top-[15%]">
+          <div className="bg-white">
+            <h3>Roster</h3>
+          </div>
+        </div>
+      )}
 
       {/* To-do Part 2 - Mobile View Card */}
       {/* Modal with side tab - contains all team athletes */}
@@ -170,7 +211,6 @@ const LineupsPage = (): JSX.Element => {
       {/* Tab visible in mobile from 320px to 448px */}
       {/* Dragonboat and tiles expand from 320px to 448 px */}
       {/* From 448px to 768px, can now bring unhide modal and move to right side as column of tiles */}
-
       {/* To-do Part 3 - Tablet + Desktop Views */}
       {/* from 768px to 1280 px, show fixed dragonboat on left and roster list on right @ 50% width share */}
       {/* Dragonboat size expands up to a fixed width ~ 50% of container */}
