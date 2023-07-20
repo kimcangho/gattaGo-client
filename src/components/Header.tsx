7a001cdd2ac1051d883eef8c8a80ae559ca37955
Kent Ho <ho.kimcang@gmail.com>
2023-07-20 18:39:35 -0400

    fix header to hide app name when logged in and hide unused schedule navlink

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -36,7 +36,9 @@ const Header = (): JSX.Element => {
         className="flex justify-center items-center space-x-2 tablet:space-x-3"
       >
         <img src={gattaGoLogo} alt="gattaGo Logo" className="h-6 tablet:h-10" />
-        <h2 className="text-gray-dark text-2xl tablet:text-3xl">gattaGo</h2>
+        {!isLoggedIn && (
+          <h2 className="text-gray-dark text-2xl tablet:text-3xl">gattaGo</h2>
+        )}
       </Link>
       {isLoggedIn && (
         <div className="flex bg-white border border-gray-border rounded">
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 import homeIcon from "../assets/icons/home-navbar.svg";
 import rosterIcon from "../assets/icons/roster-navbar.svg";
 import lineupsIcon from "../assets/icons/lineups-navbar.svg";
-import scheduleIcon from "../assets/icons/calendar-navbar.svg";
+// import scheduleIcon from "../assets/icons/calendar-navbar.svg";
 
 interface NavBarProps {
   teamId: string;
@@ -13,32 +13,42 @@ const NavBar = ({ teamId }: NavBarProps) => {
     <div className="flex mx-auto justify-center desktop:max-w-[1280px] text-white border-2 border-blue-dark rounded-b-2xl shadow-lg">
       <NavLink
         to={`../:userId/dashboard/${teamId}`}
-        className={`flex flex-col items-center p-2 tablet:p-4 bg-blue-light hover:bg-blue-dark w-[25%] rounded-bl-xl border-r border-blue-dark`}
+        className={`flex flex-col items-center p-2 tablet:p-4 bg-blue-light hover:bg-blue-dark w-[33%] rounded-bl-xl border-r border-blue-dark`}
       >
         <img src={homeIcon} alt="Home" className="w-6 tablet:w-8" />
         <p className="tablet:text-xl">Dashboard</p>
       </NavLink>
       <NavLink
         to={`../:userId/roster/${teamId}`}
-        className="flex flex-col items-center p-2 tablet:p-4 bg-blue-light hover:bg-blue-dark w-[25%] border-x border-blue-dark"
+        className="flex flex-col items-center p-2 tablet:p-4 bg-blue-light hover:bg-blue-dark w-[34%] border-x border-blue-dark"
       >
         <img src={rosterIcon} alt="Roster" className="w-6 tablet:w-8" />
         <p className="tablet:text-xl">Roster</p>
       </NavLink>
-      <NavLink
+      
+      {/* <NavLink
         to={`../:userId/lineups/${teamId}`}
         className="flex flex-col items-center p-2 tablet:p-4 bg-blue-light hover:bg-blue-dark w-[25%] border-x border-blue-dark"
       >
         <img src={lineupsIcon} alt="Lineups" className="w-6 tablet:w-8" />
         <p className="tablet:text-xl">Lineups</p>
-      </NavLink>
+      </NavLink> */}
+
       <NavLink
+        to={`../:userId/lineups/${teamId}`}
+        className="flex flex-col items-center p-2 tablet:p-4 bg-blue-light hover:bg-blue-dark w-[33%] rounded-br-xl border-l border-blue-dark"
+      >
+       <img src={lineupsIcon} alt="Lineups" className="w-6 tablet:w-8" />
+        <p className="tablet:text-xl">Lineups</p>
+      </NavLink>
+
+      {/* <NavLink
         to={`../:userId/schedule/${teamId}`}
         className="flex flex-col items-center p-2 tablet:p-4 bg-blue-light hover:bg-blue-dark w-[25%] rounded-br-xl border-l border-blue-dark"
       >
         <img src={scheduleIcon} alt="Schedule" className="w-6 tablet:w-8" />
         <p className="tablet:text-xl">Schedule</p>
-      </NavLink>
+      </NavLink> */}
     </div>
   );
 };
