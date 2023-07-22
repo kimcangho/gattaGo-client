d6c001f0c97c82c23a410f79f0652760dbdca6b3
Kent Ho <ho.kimcang@gmail.com>
2023-07-22 01:27:09 -0400

    fix bug for rendering empty seats in lineup page

--- src/utils/transformLineupsToSeats.tsx
+++ src/utils/transformLineupsToSeats.tsx
@@ -1,5 +1,9 @@
 export const transformLineupsToSeats = (lineupArr: any[]) => {
   const lineupWithSeatsArr: any[] = [];
+  const trackerArr = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21,
+  ];
 
   lineupArr.forEach((paddler) => {
     if (paddler.position === 0 || paddler.position === 21)
@@ -8,6 +12,44 @@ export const transformLineupsToSeats = (lineupArr: any[]) => {
       const rowInBoat = Math.ceil(paddler.position / 2);
       if (!lineupWithSeatsArr[rowInBoat]) lineupWithSeatsArr[rowInBoat] = [];
       lineupWithSeatsArr[rowInBoat].push(paddler);
+    }
+
+    const index = trackerArr.indexOf(paddler.position);
+    trackerArr.splice(index, 1);
+  });
+
+  trackerArr.forEach((position) => {
+    const insertRow = Math.ceil(position / 2);
+
+    if (position === 0 || position === 21) {
+      lineupWithSeatsArr[insertRow] = [
+        {
+          athlete: { isEmpty: true },
+          athleteId: null,
+          id: null,
+          position,
+          updatedAt: null,
+        },
+      ];
+    } else {
+      if (!lineupWithSeatsArr[insertRow]) lineupWithSeatsArr[insertRow] = [];
+      if (position % 2 === 1) {
+        lineupWithSeatsArr[insertRow].unshift({
+          athlete: { isEmpty: true },
+          athleteId: null,
+          id: null,
+          position,
+          updatedAt: null,
+        });
+      } else {
+        lineupWithSeatsArr[insertRow].push({
+          athlete: { isEmpty: true },
+          athleteId: null,
+          id: null,
+          position,
+          updatedAt: null,
+        });
+      }
     }
   });
 
