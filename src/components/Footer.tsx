ce92f3606285cb78aaf05e060cf4ca627d7b7045
Kent Ho <ho.kimcang@gmail.com>
2023-08-09 21:02:48 -0400

    add Github, LinkedIn and Gmail links to footer for personal contact

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,36 @@
+import githubLogo from "../assets/logos/github.svg";
+import linkedInLogo from "../assets/logos/linkedin.svg";
+import gmailLogo from "../assets/logos/gmail.svg";
+
 const Footer = (): JSX.Element => {
   return (
-    <p className="mb-4 text-center">Copyright @ 2023</p>
-  )
-}
+    <div>
+      <div className="flex justify-center space-x-4 midMobile:space-x-6 tablet:space-x-8 desktop:space-x-12 my-4 tablet:my-8 ">
+        <a href="https://github.com/kimcangho" target="_blank">
+          <img
+            src={githubLogo}
+            alt="Github"
+            className="w-6 midMobile:w-8 tablet:w-10 desktop:w-12 opacity-50 hover:opacity-100 cursor-pointer"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/kentkcho/" target="_blank">
+          <img
+            src={linkedInLogo}
+            alt="LinkedIn"
+            className="w-6 midMobile:w-8 tablet:w-10 desktop:w-12 opacity-50 hover:opacity-100 cursor-pointer"
+          />
+        </a>
+        <a href="mailto:ho.kimcang@gmail.com?subject=gattaGo Feedback">
+          <img
+            src={gmailLogo}
+            alt="Gmail"
+            className="w-6 midMobile:w-8 tablet:w-10 desktop:w-12 opacity-50 hover:opacity-100 cursor-pointer"
+          />
+        </a>
+      </div>
+      <p className="mb-4 text-center">Copyright @ 2023</p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
