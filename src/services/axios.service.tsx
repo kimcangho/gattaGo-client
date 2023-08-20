8dec1db632f5064cc9067d37e0859a72e5c011dc
Kent Ho <ho.kimcang@gmail.com>
2023-08-20 09:34:05 -0400

    set universal baseURL for API and Auth requests

--- src/services/axios.service.tsx
+++ src/services/axios.service.tsx
@@ -1,24 +1,22 @@
 import axios from "axios";
 
-const baseURLAPI = "http://localhost:8888";
-const baseURLAuth = "http://localhost:7777";
+const baseURL = "http://localhost:8888";
 
 //  Public API requests
 export default axios.create({
-  baseURL: baseURLAPI,
+  baseURL,
 });
-
 
 //  Private API requests - send authorization header with access token
 export const axiosPrivate = axios.create({
-    baseURL: baseURLAPI,
-    headers: {
-        "Content-Type": "application/json",
-    },
-    withCredentials: true,
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
 });
 
 //  Auth server requests
 export const axiosAuth = axios.create({
-  baseURL: baseURLAuth,
+  baseURL,
 });
