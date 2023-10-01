bd160088bf1fba0672e30af3ec697ba77035a2ca
Kent Ho <ho.kimcang@gmail.com>
2023-09-30 22:12:39 -0400

    change baseURL for external calls

--- src/services/axios.service.tsx
+++ src/services/axios.service.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:8888";
+// const baseURL = "http://localhost:8888";
+const baseURL = "https://gattago-server-b48a665143cc.herokuapp.com/"
 
 //  Public API requests
 export default axios.create({ baseURL });
